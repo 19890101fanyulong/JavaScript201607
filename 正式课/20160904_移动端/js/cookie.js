dad970c9b7c1c64af8183426cc98d130d95b20f0
zhouxiaotian <1282347298@qq.com>
2016-09-04 19:27:07 +0800

    submit

--- 正式课/20160904_移动端/js/cookie.js
+++ 正式课/20160904_移动端/js/cookie.js
@@ -1,18 +1,50 @@
-var cookie = {
-    set: function (name, value, expires, path, domain) {
-        expires = expires || new Date(new Date().getTime() + (1000 * 60 * 60 * 24));
-        document.cookie = name + "=" + escape(value) + ((expires) ? ";expires=" + expires.toGMTString() : "") + ((path) ? ";path=" + path : ";path=/") + ((domain) ? ";domain=" + domain : "");
-    },
-    get: function (name) {
+var cookieRender = (function () {
+    //->设置
+    function setValue(options) {
+        var _default = {
+            name: null,
+            value: null,
+            expires: new Date(new Date().getTime() + (1000 * 60 * 60 * 24)),
+            path: '/',
+            domain: ''
+        };
+        for (var key in options) {
+            if (options.hasOwnProperty(key)) {
+                _default[key] = options[key];
+            }
+        }
+        document.cookie = _default.name + "=" + escape(_default.value) + ";expires=" + _default.expires.toGMTString() + ";path=" + _default.path + ";domain=" + _default.domain;
+    }
+
+    //->获取
+    function getValue(name) {
         var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
         if (arr != null) {
             return unescape(arr[2]);
         }
         return null;
-    },
-    remove: function (name, path, domain) {
-        if (this.get(name)) {
-            document.cookie = name + "=" + ((path) ? "; path=" + path : "; path=/") + ((domain) ? "; domain=" + domain : "") + ";expires=Fri, 02-Jan-1970 00:00:00 GMT";
+    }
+
+    //->删除
+    function removeValue(options) {
+        var _default = {
+            name: null,
+            path: '/',
+            domain: ''
+        };
+        for (var key in options) {
+            if (options.hasOwnProperty(key)) {
+                _default[key] = options[key];
+            }
+        }
+        if (getValue(_default.name)) {
+            document.cookie = _default.name + "= ;path=" + _default.path + ";domain=" + _default.domain + ";expires=Fri,02-Jan-1970 00:00:00 GMT";
         }
     }
-};
+
+    return {
+        set: setValue,
+        get: getValue,
+        remove: removeValue
+    }
+})();
